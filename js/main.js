53f5e381d3d1d799c99a1e84bd28af69d95b3923
pranit ingole <pranitingole1@gmal.com>
2021-07-01 02:48:49 +0530

    error solve

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 const fetchUsers = async () =>
-// await (await fetch('/.netlify/functions/getusers')).json();
-await (await fetch('http://localhost:9000/getusers')).json();
+await (await fetch('/.netlify/functions/getusers')).json();
+// await (await fetch('http://localhost:9000/getusers')).json();
 fetchUsers().then(data => {
 userList = document.querySelector('#users');
 
